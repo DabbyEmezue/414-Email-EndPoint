124d2bc24a0720ab047c5f7e1a3412b31384277b
Dabby Emezue <emezued@gmail.com>
2023-05-07 15:22:36 +0100

    Port was changed from hardcode"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const emailModel = require("./model");
-const PORT = 3000;
+let PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
